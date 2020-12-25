716d2faca329969812f04a2cfd015413ae9c00c8
Diego <diego.a.leon24@gmail.com>
2020-12-24 19:55:05 -0500

    test: refactory VehicleModel

--- src/_tests_/vehicle-model.spec.ts
+++ src/_tests_/vehicle-model.spec.ts
@@ -1,3 +1,4 @@
+import { VehicleHelper } from '../helpers/vehicle-model-helper'
 import { VehicleModel } from '../models/vehicle-model'
 
 describe('Classes', () => {
@@ -10,5 +11,10 @@ describe('Classes', () => {
     expect(vehicleModel.getName()).toEqual('Nissan')
     expect(vehicleModel.getModel()).toEqual('DXT')
     expect(vehicleModel.getYear()).toEqual(2020)
+  })
+
+  it('Class VehicleHelper', () => {
+    const vehicleHelper = new VehicleHelper()
+    expect(vehicleHelper).toEqual(vehicleHelper)
   })
 })
